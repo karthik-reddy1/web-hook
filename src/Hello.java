03f336b0d1bf11cabd3f049820cc3e9611016320
Karthik Reddy Vippakuntla <karthikreddyvippakuntla@gmail.com>
2025-11-30 17:01:50 +0530

    fn

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hello rahul!");
+    System.out.println("Hello ugyuerige");
+    
+    System.out.println("changes in the file");
+
+  }
+}
