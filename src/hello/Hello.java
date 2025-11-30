9a5a9f65f6d25929b22e7419b21c821a1b9c72a1
Karthik Reddy Vippakuntla <karthikreddyvippakuntla@gmail.com>
2025-11-30 17:19:35 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,7 +2,7 @@ package hello;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
-    System.out.println("Hello rahul 2nd time!");
+    System.out.println("Hello rahul 2nd timeeeee!");
     System.out.println("Hello ugyuerige");
     
     System.out.println("changes in the file");
